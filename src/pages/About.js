e5d93a357066c71d3b166fb53282205644f3b052
ridhorp <ridhogoodevil@gmail.com>
2022-07-21 17:22:05 +0700

    check token

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,18 +1,38 @@
-import React, { Component, Fragment } from "react";
-import { Container } from "reactstrap";
+import React, { Fragment, useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { Container, Button } from "reactstrap";
 import Navbar from "../components/NavbarComponent";
 
-class About extends Component {
-  render() {
-    return (
-      <Fragment>
-        <Navbar />
-        <Container>
-          <h1>About</h1>
-        </Container>
-      </Fragment>
-    );
-  }
+function About() {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    checkUser();
+  });
+
+  const checkUser = () => {
+    const token = localStorage.getItem("token");
+    if (!!token) return setIsAuthenticated(true);
+    navigate("/login");
+  };
+
+  const handleLogout = () => {
+    localStorage.removeItem("token");
+    navigate("/login");
+  };
+
+  return (
+    <Fragment>
+      <Navbar />
+      <Container>
+        <h1>Halaman About</h1>
+        <Button color="danger" onClick={handleLogout}>
+          Logout
+        </Button>
+      </Container>
+    </Fragment>
+  );
 }
 
 export default About;
